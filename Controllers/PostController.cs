1d297643d4f216fbe8c24db002598899db83ee8b
Berrin <berrinsevin8@gmail.com>
2023-07-18 17:49:47 +0300

    ToDo Backend - Katmansız api

--- Controllers/PostController.cs
+++ Controllers/PostController.cs
@@ -1,3 +1,5 @@
+using todo.Api.Dtos;
+using todo.Api.Helpers;
 using todo.Api.Entities;
 using todo.Api.Repositories;
 using Microsoft.AspNetCore.Mvc;
@@ -24,21 +26,69 @@ namespace todo.Api.Controllers
         // }
 
         [HttpGet("getPosts")]
-        public IEnumerable<Post> GetPosts()
+        public IEnumerable<PostDto> GetPosts()
         {
-            var posts = repository.GetPosts();
+            var posts = repository.GetPosts().Select(x => x.Mapper());
             return posts;
         }
 
         [HttpGet("getPost/{id}")]
-        public ActionResult<Post> GetPost(Guid id)
+        public ActionResult<PostDto> GetPost(Guid id)
         {
-            var post = repository.GetPost(id);
+            var post = repository.GetPost(id).Mapper();
+
             if (post == null)
             {
                 return NotFound(post);
             }
+
             return post;
+        }
+
+        [HttpPost("createPost")]
+        public ActionResult<PostDto> CreatePost(CreatePostDto createPostDto)
+        {
+            Post post = new()
+            {
+                Id = Guid.NewGuid(),
+                UserCode = createPostDto.UserCode,
+                EntryDate = DateTime.Today,
+                ChannelCode = createPostDto.ChannelCode,
+                Notes = createPostDto.Notes,
+                StatusCode = createPostDto.StatusCode,
+                ExpireDate = createPostDto.ExpireDate
+            };
+
+            repository.CreatePost(post);
+
+            //döndürülen nesnenin oluşturulma bilgilerini getirir
+            return CreatedAtAction(nameof(GetPost), new { id = post.Id }, post.Mapper());
+        }
+
+        [HttpPut("updatePost/{id}")]
+        public ActionResult<PostDto> UpdateItem(Guid id, UpdatePostDto updatePostDto)
+        {
+            var existingPost = repository.GetPost(id);
+
+            if (existingPost == null)
+            {
+                return NotFound();
+            }
+
+            Post post = new()
+            {
+                Id = id,
+                UserCode = updatePostDto.UserCode,
+                EntryDate = existingPost.EntryDate,
+                ChannelCode = updatePostDto.ChannelCode,
+                Notes = updatePostDto.Notes,
+                StatusCode = updatePostDto.StatusCode,
+                ExpireDate = updatePostDto.ExpireDate
+            };
+
+            repository.UpdatePost(post);
+
+            return CreatedAtAction(nameof(GetPost), new { id = post.Id }, post.Mapper());
         }
     }
 
